9c5300c5c204a79a967d5e844a00b78d7d008311
priscilla-e <56302477+priscilla-e@users.noreply.github.com>
2023-11-24 06:17:59 +0000

    Add game header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header className="flex flex-col items-center justify-center space-y-4 my-10">
+            <img src="/game-logo.png" alt="Game Logo" className="h-24 w-24"/>
+            <h1 className="text-4xl text-earth font-cursive">Tic-Tac-Toe</h1>
+        </header>
+    );
+}
