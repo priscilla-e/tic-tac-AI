6b215781c50e590b8b6ab4200148d88620504f96
Priscilla Emasoga <priscillaemasoga@gmail.com>
2023-11-26 23:13:49 +0000

    Add game play audios

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,16 +1,27 @@
+import {useRef} from 'react'
+import clickSound from "../assets/click-sound.wav"
+
 
 export default function GameBoard({board, onSelect}) {
+    const clickAudioRef = useRef(new Audio(clickSound));
+
+    const handleClick = (row, col) => {
+        console.log(clickAudioRef);
+        clickAudioRef.current.play().catch(error => console.error('Error playing sound:', error));
+        onSelect(row, col);
+    };
+       
 
     return (
         <div
-            className={`grid grid-cols-${board.length} gap-3 max-w-lg mx-auto mt-8 md:gap-6`}
+            className={`grid grid-cols-3 gap-3 max-w-lg mx-auto mt-8 md:gap-6`}
         >
             {board.map((row, rowIndex) =>
                 row.map((col, colIndex) => (
                     <button
                         key={`cell-${rowIndex},${colIndex}`}
                         className="bg-smoke font-cursive h-28 text-6xl text-earth uppercase md:text-7xl md:h-32"
-                        onClick={() => onSelect(rowIndex, colIndex)}
+                        onClick={() => handleClick(rowIndex, colIndex)}
                         disabled={!!board[rowIndex][colIndex]}
                     >
                         {col}
--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -1,9 +1,24 @@
+import { useRef } from 'react';
+import winSound from '../assets/win-sound.wav';
+import drawSound from '../assets/draw-sound.wav';
+
+
 export default function GameOver({ winner, onRematch }) {
+    const winAudioRef = useRef(new Audio(winSound));
+    const drawAudioRef = useRef(new Audio(drawSound));
+    
+    if (winner) {
+        winAudioRef.current.play();
+    }
+    else {
+        drawAudioRef.current.play();
+    }
+
     return (
         <div className="absolute top-0 bottom-0 left-0 right-0 flex flex-col space-y-8 items-center justify-center text-smoke bg-black bg-opacity-80 animate-pop-in">
             <p className="font-cursive text-4xl text-lightYellow">Game Over!</p>
-            {winner ?  <p>{`${winner} Won!`}</p> : <p>{"It's a Draw!"}</p>}
-           
+            {winner ? <p>{`${winner} Won!`}</p> : <p>{"It's a Draw!"}</p>}
+
             <button
                 className="px-4 py-1 border-2  border-darkYellow text-darkYellow rounded-sm animate-bounce focus:outline-none"
                 onClick={onRematch}
