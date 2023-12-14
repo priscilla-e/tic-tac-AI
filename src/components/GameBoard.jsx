61abc859a8ba81d7f4d21366464a52463ae80594
priscilla-e <56302477+priscilla-e@users.noreply.github.com>
2023-12-14 21:21:47 +0000

    Refactor gameboard component

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,13 +1,5 @@
 export default function GameBoard({board, onSelect, isComTurn}) {
-    const handleClick = (row, col) => {
-        onSelect(row, col);
-    };
 
-    // let classes = "grid grid-cols-3 gap-3 max-w-lg mx-auto mt-8 md:gap-6"
-    // if (board.length === 4) {
-    //     classes = "grid grid-cols-4 gap-3 max-w-lg mx-auto mt-8 md:gap-6"
-    // }
-       
     return (
         <div
             className={`grid grid-cols-${board.length.toString()} gap-3 max-w-lg mx-auto mt-8 md:gap-6`}
@@ -17,7 +9,7 @@ export default function GameBoard({board, onSelect, isComTurn}) {
                     <button
                         key={`cell-${rowIndex},${colIndex}`}
                         className="bg-smoke font-cursive h-28 text-6xl text-earth uppercase md:text-7xl md:h-32"
-                        onClick={() => handleClick(rowIndex, colIndex)}
+                        onClick={() => onSelect(rowIndex, colIndex)}
                         disabled={!!board[rowIndex][colIndex] || isComTurn}
                     >
                         {col}
