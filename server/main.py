37ea5fd19cc7bbca991ca84fa9f0892561a233e8
priscilla-e <56302477+priscilla-e@users.noreply.github.com>
2023-12-17 14:20:58 +0000

    Add backend server for GPT feature

---
+++ server/main.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI
+from openai import OpenAI
+from config import settings
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173/", "https://localhost:5173/"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+client = OpenAI(api_key=settings.openai_key, organization=settings.organization)
+
+
+@app.get("/")
+async def root():
+    return {"message": "API is running..."}
+
+
+@app.get("/gpt_move")
+async def get_gpt_move(board: str, model: str = "gpt-3.5-turbo-1106"):
+    response = client.chat.completions.create(
+        # model="gpt-4-1106-preview",
+        model=model,
+        temperature=0.3,
+        response_format={"type": "json_object"},
+        messages=[
+            {"role": "system",
+             "content": "You are an opponent in a Tic-Tac-Toe game. You're playing as 'O' and your goal is to p. Suggest the indexes of the next move as 'row,col' in JSON format"},
+            {"role": "user",
+             "content": f"Given the current Tic-Tac-Toe board:\n{board}\nMake the next move for 'O':"},
+        ],
+    )
+    move = response.choices[0].message.content
+    return move
