c3f79b54c3da2456a77814931a52f0bf71de6f85
priscilla-e <56302477+priscilla-e@users.noreply.github.com>
2024-01-01 11:17:05 +0000

    Add BaseCard component and stying

---
+++ client/src/components/shared/BaseCard.jsx
@@ -0,0 +1,13 @@
+import PropTypes from "prop-types";
+
+export default function BaseCard({children}) {
+    return (
+        <div className="relative mx-auto px-2 py-8 w-full bg-white shadow-custom rounded-md bg-gradient-to-b from-earth to-darkEarth md:max-w-2xl md:px-8 md:py-12">
+            {children}
+        </div>
+    )
+}
+
+BaseCard.propTypes = {
+    children: PropTypes.node.isRequired,
+}
