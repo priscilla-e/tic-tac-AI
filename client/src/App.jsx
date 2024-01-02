95cf997bd13a3d3c08b3f80fedd4a0774c129f0f
priscilla-e <56302477+priscilla-e@users.noreply.github.com>
2024-01-02 21:07:26 +0000

    Refactor components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import {useContext} from 'react';
-import Header from './components/Header.jsx';
+import Header from './components/layout/Header.jsx';
 import PlayNow from './components/PlayNow.jsx';
-import GameMode from './components/GameMode.jsx';
+import SelectMode from './components/SelectMode.jsx';
 import GameContext from './contexts/game-context.jsx';
 
 function App() {
@@ -18,7 +18,7 @@ function App() {
             <Header/>
             <main className="container mx-auto px-2">
                 {ctx.page === 0 && <PlayNow onPlay={() => ctx.setPage(1)}/>}
-                {ctx.page === 1 && <GameMode onSelectMode={handleGameMode}/>}
+                {ctx.page === 1 && <SelectMode onSelectMode={handleGameMode}/>}
             </main>
         </>
     );
---
+++ client/src/AppBackup.jsx
@@ -0,0 +1,159 @@
+import { useEffect, useState } from 'react';
+import Header from './components/layout/Header.jsx';
+import Player from './components/legacy/Player.jsx';
+import GameBoard from './components/legacy/GameBoard.jsx';
+import GameOver from './components/GameOver.jsx';
+import GameSettings from './components/legacy/GameSettings.jsx';
+
+import {
+    checkWinner,
+    checkDraw,
+    createEmptyGameBoard,
+    getRandomEmptyCell,
+    playAudio,
+    getMoveFromGPT,
+    findBestMove,
+} from './utils.js';
+
+import clickSound from './assets/click-sound.wav';
+
+const DEFAULT_SETTINGS = {
+    mode: 'GPT', //GPT, MINIMAX, MULTI_PLAYER
+    boardSize: 3,
+    allowAudio: true,
+};
+
+const COM_NAMES = ['GPT', 'MINIMAX'];
+
+let comMoveTimeout;
+
+function App() {
+    const [gameSettings, setGameSettings] = useState(DEFAULT_SETTINGS);
+    const [gameBoard, setGameBoard] = useState(
+        createEmptyGameBoard(gameSettings.boardSize)
+    );
+    const [players, setPlayers] = useState({
+        X: 'Player 1',
+        O: gameSettings.mode,
+    });
+    const [currentPlayer, setCurrentPlayer] = useState('X');
+
+    let winner = checkWinner(gameBoard);
+    let isDraw = !winner && checkDraw(gameBoard);
+    let isComTurn = !winner && COM_NAMES.includes(players[currentPlayer]);
+
+    // reset the game if the game settings change
+    useEffect(() => {
+        handleRematch();
+    }, [gameSettings]);
+
+    useEffect(() => {
+        if (isComTurn) {
+            if (gameSettings.mode === 'GPT') {
+                // model: "gpt-4-1106-preview",
+                const move =  getMoveFromGPT(gameBoard, "gpt-3.5-turbo-1106");
+                move.then((move) => {
+                    handleSelect(move.row, move.col);
+                });
+            } else if (gameSettings.mode === 'MINIMAX') {
+                comMoveTimeout = setTimeout(() => {
+                    const move = findBestMove(gameBoard);
+                    handleSelect(move.row, move.col);
+                }, 1000);
+            }
+
+            return () => clearTimeout(comMoveTimeout);
+        }
+    }, [isComTurn, gameBoard, gameSettings.mode]);
+
+    const handleSelect = (row, col) => {
+        setGameBoard((prevBoard) => {
+            const newBoard = prevBoard.map((row) => [...row]);
+            newBoard[row][col] = currentPlayer;
+
+            return newBoard;
+        });
+
+        // Play click sound
+        if (gameSettings.allowAudio) {
+            playAudio(clickSound);
+        }
+
+        // Switch player
+        if (currentPlayer === 'X') {
+            setCurrentPlayer('O');
+        } else {
+            setCurrentPlayer('X');
+        }
+    };
+
+    const handleNameChange = (symbol, newName) => {
+        setPlayers((prevPlayers) => {
+            return { ...prevPlayers, [symbol]: newName };
+        });
+    };
+
+    const handleRematch = () => {
+        setGameBoard(createEmptyGameBoard(gameSettings.boardSize));
+        setCurrentPlayer('X');
+
+        //  Player 2 depending on the game mode
+        if (gameSettings.mode === 'MULTI') {
+            setPlayers((prevPlayers) => {
+                return { ...prevPlayers, O: 'Player 2' };
+            });
+        } else {
+            setPlayers((prevPlayers) => {
+                return { ...prevPlayers, O: gameSettings.mode };
+            });
+        }
+    };
+
+    return (
+        <>
+            <Header />
+            <main className="container mx-auto px-2">
+                {/*Game Container Card */}
+                <div className="relative mx-auto px-2 py-8 w-full bg-white shadow-custom rounded-md bg-gradient-to-b from-earth to-darkEarth md:max-w-2xl md:px-8 md:py-12">
+                    {(winner || isDraw) && (
+                        <GameOver
+                            winner={winner ? players[winner] : null}
+                            gameSettings={gameSettings}
+                            onRematch={handleRematch}
+                        />
+                    )}
+                    {/*Player Names Container*/}
+                    <div className="flex space-x-2 md:space-x-10">
+                        <Player
+                            defaultName={players.X}
+                            symbol="X"
+                            isActive={currentPlayer == 'X'}
+                            onSave={handleNameChange}
+                        />
+                        <Player
+                            defaultName={players.O}
+                            symbol="O"
+                            isActive={currentPlayer == 'O'}
+                            onSave={handleNameChange}
+                        />
+                    </div>
+
+                    <GameBoard
+                        board={gameBoard}
+                        isComTurn={isComTurn}
+                        onSelect={handleSelect}
+                    />
+                </div>
+
+                {/*<GameSettings*/}
+                {/*    initialSettings={gameSettings}*/}
+                {/*    onSettingsChange={(newSettings) =>*/}
+                {/*        setGameSettings(newSettings)*/}
+                {/*    }*/}
+                {/*/>*/}
+            </main>
+        </>
+    );
+}
+
+export default App;
