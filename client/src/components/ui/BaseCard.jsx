ceb6c083026c92d497181ab8e24455b6cfbdb244
priscilla-e <56302477+priscilla-e@users.noreply.github.com>
2024-01-02 18:53:37 +0000

    Add PlayNow component

--- client/src/components/ui/BaseCard.jsx
+++ client/src/components/ui/BaseCard.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 
 export default function BaseCard({children}) {
     return (
-        <div className="relative mx-auto px-2 py-8 w-full bg-white shadow-custom rounded-md bg-gradient-to-b from-earth to-darkEarth md:max-w-2xl md:px-8 md:py-12">
+        <div className="relative text-gray-300 mx-auto px-2 py-8 w-full bg-white shadow-custom rounded-md bg-gradient-to-b from-earth to-darkEarth md:max-w-2xl md:px-8 md:py-12">
             {children}
         </div>
     )
