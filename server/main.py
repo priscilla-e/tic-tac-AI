d2b65038f84e41120764a1fb40aed38aa4b847f7
priscilla-e <56302477+priscilla-e@users.noreply.github.com>
2024-01-04 17:23:43 +0000

    Add minimax move endpoint to server

--- server/main.py
+++ server/main.py
@@ -3,6 +3,8 @@ from openai import OpenAI
 from config import settings
 from fastapi.middleware.cors import CORSMiddleware
 import json
+from utils import find_best_move
+
 
 app = FastAPI()
 app.add_middleware(
@@ -23,6 +25,12 @@ async def root():
 
 @app.get("/gpt-move")
 async def get_gpt_move(board, model: str = "gpt-3.5-turbo-1106"):
+    """
+    Use the GPT engine to generate a move for the AI player.
+    :param board: the game board  as a 2D list
+    :param model: the GPT model to use (gpt-3.5-turbo-1106 OR gpt-4.5-turbo-1106)
+    :return:
+    """
     board = json.loads(board)
     size = len(board)
 
@@ -39,3 +47,16 @@ async def get_gpt_move(board, model: str = "gpt-3.5-turbo-1106"):
     )
     move = response.choices[0].message.content
     return move
+
+
+@app.get("/minimax-move")
+def get_minimax_move(board):
+    """
+    Find the best move for the AI player using the minimax algorithm.
+    :param board: the game board  as a 2D list
+    :return: a dictionary with keys 'row' and 'col'
+    """
+    board = json.loads(board)
+
+    print(board)
+    return find_best_move(board, 3)
