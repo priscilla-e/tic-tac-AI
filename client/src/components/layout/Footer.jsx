df0fc26e5abf9122da2796e63925e9b690ab5ccb
priscilla-e <56302477+priscilla-e@users.noreply.github.com>
2024-01-05 22:38:14 +0000

    Fix mobile styling issues

--- client/src/components/layout/Footer.jsx
+++ client/src/components/layout/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer className='flex flex-col mt-24 justify-center items-center'>
+        <footer className='absolute bottom-0 left-1/2 -translate-x-1/2 w-full flex flex-col mt-24 justify-center items-center'>
             <p> Developed by <a  className='underline' href='https://github.com/priscilla-e'> Priscilla Emasoga.</a> </p>
             <p> © All rights reserved.</p>
         </footer>
